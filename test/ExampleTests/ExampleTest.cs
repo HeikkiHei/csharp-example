afde72edf6df875143ba8a09d1b38f2293299917
HeikkiHei <heikkihei.ahonen@gmail.com>
2020-03-26 13:25:06 +0200

    Add test to test

--- test/ExampleTests/ExampleTest.cs
+++ test/ExampleTests/ExampleTest.cs
@@ -29,5 +29,27 @@ namespace ExampleTests
         Assert.AreEqual("Hello World!\n", sw.ToString().Replace("\r\n", "\n"), "Check your code! The test requires exact match!");
       }
     }
+
+    [Test]
+    public void TestExampleNegated()
+    {
+      using (StringWriter sw = new StringWriter())
+      {
+        // Save a reference to the standard output.
+        TextWriter stdout = Console.Out;
+
+        // Redirect standard output to variable.
+        Console.SetOut(sw);
+
+        // Call student's code
+        Program.Main(null);
+
+        // Restore the original standard output.
+        Console.SetOut(stdout);
+
+        // Assert
+        Assert.AreNotEqual("Hei Maailma!\n", sw.ToString().Replace("\r\n", "\n"), "Check your code! The test requires exact match!");
+      }
+    }
   }
 }
