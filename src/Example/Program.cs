e3c2eae30f4cb0f0f9e6d16bd77e012fc12a77c6
HeikkiHei <heikkihei.ahonen@gmail.com>
2020-04-01 10:26:19 +0300

    Break the code

--- src/Example/Program.cs
+++ src/Example/Program.cs
@@ -6,7 +6,7 @@ namespace Example
   {
     public static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      Console.WriteLine("Hello Pietarsaari and Kokkola!");
     }
   }
 }
